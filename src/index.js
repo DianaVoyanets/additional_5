ac398040a0811092b2cc68ae0742d02edace08a2
d.voyanets <diana.voyanets@gmail.com>
2018-02-24 00:10:03 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var openingBrackets = {},
+      closingBrackets = {},
+      poll = [];
+
+  bracketsConfig.forEach((bracket) => { 
+      openingBrackets[bracket[0]] = bracket[1];
+      closingBrackets[bracket[1]] = bracket[0];
+  });
+      
+  for (var i = 0; i < str.length; i++) {
+      var bracket = str[i],
+          endBracket = closingBrackets[bracket];
+
+      if (endBracket && (bracket !== endBracket || poll[poll.length - 1] === endBracket)) {
+          endBracket = poll.pop();
+
+          if (bracket !== openingBrackets[endBracket]) {
+              return false;
+          }
+      } else {
+          poll.push(bracket);
+      }
+  }
+
+  return poll.length === 0;
 }
